857215760ad155d879ad6ffdbd44175f83f9c20b
Andrii Poliukhovych <andrii.poliukhovych.s@gmail.com>
2020-07-05 08:31:19 +0300

    Add my setting

--- Program.cs
+++ Program.cs
@@ -7,13 +7,17 @@ namespace azure_app_configuration_demo
     class Program
     {
         const string ConnectionString = "Endpoint=https://appconfigurationfortest.azconfig.io;Id=3+ar-l0-s0:x+k3i+fPcIP55+86uGn5;Secret=RRTcyLITVEhM1llwoXeCmHwIE8LHNGc7YR5+CddUoos=";
+
+        const string emptyValue = "No value was found for the setting.";
+
         static void Main(string[] args)
         {
             var builder = new ConfigurationBuilder();
             builder.AddAzureAppConfiguration(ConnectionString);
 
             var config = builder.Build();
-            Console.WriteLine(config["TestApp:Settings:Message"] ?? "Hello world!");
+            Console.WriteLine(config["TestApp:Settings:Message"] ?? emptyValue);
+            Console.WriteLine(config["Message"] ?? emptyValue);
         }
     }
 }
